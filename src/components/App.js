1e62eda2fa4f4d26d38c0a7d43b6d502d9d327e1
rhemanth399 <120007414+rhemanth399@users.noreply.github.com>
2023-03-23 19:41:31 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,28 @@
 
 import React from "react";
 import './../styles/App.css';
-
+import {useState} from 'react'
 const App = () => {
+  const [showModal,setshowModal]=useState(false);
+  const [modalContent,setModalContent]=useState(null);
+  function clickBtn(){
+    
+    setModalContent(
+      <div>
+    <h1>Modal Content</h1>
+    <p>This is the modal content</p>
+    </div>
+    )
+    setshowModal(true)
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      <div><h1>Parent Component</h1>
+      </div>
+        <div><h1>Child Component</h1>
+        <button onClick={clickBtn}>Show Modal</button>
+        </div>
+        {showModal&&<div className="modal">{modalContent}</div>}
     </div>
   )
 }
